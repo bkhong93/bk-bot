d40af6d394c3da55073b8fb17f69d3735c6adf84
bkhong93 <48567674+bkhong93@users.noreply.github.com>
2019-09-21 14:20:25 -0700

    Delete roll.js

--- commands/roll.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	name: 'roll',
-	description: 'roll against bot',
-	execute(msg) {
-        var playerRoll = Math.round(((Math.random() * 999) + 1));
-            msg.channel.send('You roll a ' + playerRoll + '.');
-	}
-};
